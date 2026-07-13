fb80bc43b0d15e5bd312374992b01024dfebb13a
yuzu1012 <yuzusonyuzuha@gmail.com>
2026-07-14 01:05:04 +0900

    [add]ダメージ情報をまとめるクラスを追加

---
+++ Assets/Script/Character/DamageContext.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+/// <summary>
+/// ダメージ情報を保持する
+/// </summary>
+public class DamageContext
+{
+    public bool IsKnockback { get; private set; }
+    public Vector3 Direction { get; private set; }
+    public float Power { get; private set; }
+    public float UpPower { get; private set; }
+    public float Duration { get; private set; }
+    
+    public DamageContext(bool isKnockback, Vector3 direction, float power, float upPower, float duration)
+    {
+        IsKnockback = isKnockback;
+        Direction = direction;
+        Power = power;
+        UpPower = upPower;
+        Duration = duration;
+    }
+}
